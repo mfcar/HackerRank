2cf6875d220ee7ad131291c987b8523ce572e7b8
Marcos Carvalho <mcarvalho@sestsenat.org.br>
2019-09-16 16:56:02 -0300

    CoutingValleys Ok

---
+++ CountingValleys/MainCountingValleys.cs
@@ -0,0 +1,31 @@
+﻿namespace HackerRank
+{
+    public class MainCountingValleys
+    {
+        public static int CountingValleys(int n, string s)
+        {
+            var actualHeight = 0;
+            var valleyCount = 0;
+            foreach (var direction in s)
+            {
+                if (direction == 'U')
+                {
+                    actualHeight++;
+                }
+                else
+                {
+                    actualHeight--;
+                }
+
+
+                // Checks if direction is down e first time in down direction
+                if (actualHeight == -1 && direction == 'D')
+                {
+                    valleyCount++;
+                }
+            }
+
+            return valleyCount;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainCountingValleys.CountingValleys(8, "UDDDUDUU");
         }
     }
 }
