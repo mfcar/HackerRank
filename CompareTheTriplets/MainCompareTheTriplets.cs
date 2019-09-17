9e30c45057dfe6be8f6ed73d8344ef7b8ebc4ed6
Marcos Carvalho <mcarvalho@sestsenat.org.br>
2019-09-17 16:32:44 -0300

    Compare the Triples Ok

---
+++ CompareTheTriplets/MainCompareTheTriplets.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.CompareTheTriplets
+{
+    public class MainCompareTheTriplets
+    {
+        public static List<int> CompareTriplets(List<int> a, List<int> b)
+        {
+            var aliceScore = 0;
+            var bobScore = 0;
+
+            for (var i = 0; i < a.Count; i++)
+            {
+                if (a[i] > b[i]) aliceScore++;
+                if (b[i] > a[i]) bobScore++;
+            }
+
+            return new List<int>{aliceScore, bobScore};
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using HackerRank.AVeryBigSum;
+﻿using System.Collections.Generic;
+using HackerRank.AVeryBigSum;
+using HackerRank.CompareTheTriplets;
 using HackerRank.RepeatedString;
 using HackerRank.Staircase;
 
@@ -8,7 +10,9 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
-            MainStaircase.Staircase(100);
+            var alice = new List<int> { 5, 6, 7 };
+            var bob = new List<int> { 3, 6, 10 };
+            MainCompareTheTriplets.CompareTriplets(alice, bob);
         }
     }
 }
