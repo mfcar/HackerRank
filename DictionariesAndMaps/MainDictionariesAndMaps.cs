87705963126621169edcc1654e92e6fab0243158
Marcos Carvalho <mcarvalho@sestsenat.org.br>
2019-09-24 11:08:47 -0300

    Day 8: Ok

---
+++ DictionariesAndMaps/MainDictionariesAndMaps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.DictionariesAndMaps
+{
+    public class MainDictionariesAndMaps
+    {
+        public static void DictionariesAndMaps()
+        {
+            var contactsCount = Convert.ToInt32(Console.ReadLine());
+            var phoneBook = new Dictionary<string, string>();
+
+            for (var i = 0; i < contactsCount; i++)
+            {
+                var contact = Console.ReadLine().Split(' ');
+                phoneBook.Add(contact[0], contact[1]);
+            }
+
+            while (true)
+            {
+                var name = Console.ReadLine();
+                Console.WriteLine(phoneBook.ContainsKey(name) ? $"{name}={phoneBook[name]}" : "Not found");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using HackerRank.BirthdayCakeCandles;
 using HackerRank.CamelCase;
