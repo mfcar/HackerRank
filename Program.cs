5b059ef7435483f5d668e4300a81dcf1a5afaa21
Marcos Carvalho <marcosfcar@gmail.com>
2019-09-25 14:37:06 -0300

    Day 9 ok

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.IO;
 using System.Linq;
 using HackerRank.BirthdayCakeCandles;
 using HackerRank.CamelCase;
@@ -12,8 +13,29 @@ namespace HackerRank
 {
     class Program
     {
+
+        // Complete the factorial function below.
+        static int factorial(int n)
+        {
+            //if (n <= 1) return n;
+            //var numeroAnterior = n - 1;
+            //var resultadoFatorial = factorial(numeroAnterior);
+            //var valorN = n * resultadoFatorial;
+            //return valorN;
+
+            if (n <= 1) return n;
+            var numeroAnterior = n - 1;
+            var resultadoFatorial = factorial(numeroAnterior);
+            var valorN = n * resultadoFatorial;
+            return n * factorial(n - 1);
+        }
+
         static void Main(string[] args)
         {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = factorial(n);
         }
     }
 }
---
+++ Recursion3/MainRecursion3.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.Recursion3
+{
+    class MainRecursion3
+    {
+    }
+}
