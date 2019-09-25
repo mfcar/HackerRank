7a8d16d6852c3a9d42d961c9413ad95843fc1755
Marcos Carvalho <marcosfcar@gmail.com>
2019-09-25 14:38:29 -0300

    Recursivo

--- Recursion3/MainRecursion3.cs
+++ Recursion3/MainRecursion3.cs
@@ -6,5 +6,11 @@ namespace HackerRank.Recursion3
 {
     class MainRecursion3
     {
+        // Complete the factorial function below.
+        static int Factorial(int n)
+        {
+            if (n <= 1) return n;
+            return n * Factorial(n - 1);
+        }
     }
 }
